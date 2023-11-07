13fa3867eb3f96991f47bb30819e2b5704ad4735
NhatTan1212 <honhattan121@gmail.com>
2023-11-07 16:27:15 +0700

    update connect azure sql

--- connect.js
+++ connect.js
@@ -1,14 +1,19 @@
 const sql = require('mssql');
+require('dotenv').config()
 
 const config = {
-    user: "sa",
-    password: "123456tk",
-    database: "QUANLYBANLAPTOP",
-    server: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_NAME,
+    authentication: {
+        type: 'default'
+    },
     options: {
-        trustServerCertificate: true
+        encrypt: true
     }
-};
+}
+console.log(config)
 
 const connect = new sql.ConnectionPool(config).connect().then(pool => {
     return pool;
